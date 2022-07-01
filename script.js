039e457bdd8d4ca038113bae109e76709c9ae120
Ricardo Borges <ricardoborges00@hotmail.com>
2022-07-01 12:24:34 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,19 @@
+for (let i=0; i<=10; i++){
+    console.log(i)
+}
+
+/*let i = 10;
+while (i >=0){
+    console.log(i);
+    i--;
+}*/
+
+for (let i=1; i<=100; i+=2){
+    console.log(i)
+}
+
+let i = 0;
+while (i <=100){
+    console.log(i);
+    i+=2;
+}
